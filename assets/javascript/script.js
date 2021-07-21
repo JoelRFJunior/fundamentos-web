7a96b7bf99ff8de76ea13e54dc1ce7b3c17d4581
JoelRFJunior <joeljr_jc@hotmail.com>
2021-07-21 10:47:22 -0300

    atualizando botão enviar e validando caixa de texto assunto

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,32 +2,58 @@ let nome = document.querySelector('#nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
 
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+
 nome.style.width = '100%';
 
 email.style.width = '100%';
 
-function validaNome(){
-   let txt = document.querySelector('#txtNome')
+function validaNome() {
+    let txt = document.querySelector('#txtNome')
     if (nome.value.length < 3) {
-        txt.innerHTML='nome invalido'
+        txt.innerHTML = 'nome invalido'
         txt.style.color = 'red'
         nome.style.border = '2px solid red'
     } else {
-        txt.innerHTML=''
+        txt.innerHTML = ''
         nome.style.border = '2px solid green'
+        nomeOk = true
     }
 }
 
-function validaEmail(){
+function validaEmail() {
     let txtEmail = document.querySelector('#txtEmail')
 
-    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') ==-1) {
-        txtEmail.innerHTML='E-mail invalido'
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML = 'E-mail invalido'
         txtEmail.style.color = 'red'
         email.style.border = '2px solid red'
     } else {
-        txtEmail.innerHTML=''
+        txtEmail.innerHTML = ''
         email.style.border = '2px solid green'
+        emailOk = true
+    }
+
+    function validaAssunto() {
+        let txtAssunto = document.querySelector("txtAssunto")
+        if (assunto.value.length > 100 && assunto.value.length > 0) {
+            txtAssunto.innerHTML = 'texto muito grande digite no máximo 100 caracteres'
+            txtAssunto.style.color = 'red'
+            txtAssunto.style.display = 'block'
+        } else {
+            txtAssunto.style.display = 'none'
+            assuntoOk = true
+        }
+    }
+
+    function enviar() {
+        if (nomeOk == true && emailOk == true && assuntoOk == true) {
+            alert("formulario enviado com sucesso!")
+        } else {
+            alert("Por Favor, preencha o formulario corretamente")
+        }
     }
 }
 
